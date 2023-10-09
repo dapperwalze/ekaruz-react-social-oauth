b6e5e4ac187324414e5abda75e76522b5752a5d0
dapperwalze <dapperwalze@Walzes-MacBook-Pro.local>
2023-10-09 05:04:04 +0100

    fix: fix twitter  callback url

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   const [googleData, setGoogleData] = useState<Record<string, any> | null>(null);
 
   const { onFacebookConnect, facebookData, isLoading: isFacebookLoading } = useFacebookConnection({})
-  const { onTwitterConnect, twitterData, isLoading } = useTwitterConnection({ clientId: process.env.RS_TWITTER_CLIENT_KEY as string, redirect_uri: `${window.location.origin}/callback/twitter}`, isOnlyGetCode: true, isOnlyGetToken: false, clientKeys: `${process.env.RS_TWITTER_CLIENT_KEYS as string}` });
+  const { onTwitterConnect, twitterData, isLoading } = useTwitterConnection({ clientId: process.env.RS_TWITTER_CLIENT_KEY as string, redirect_uri: `${window.location.origin}/callback/twitter`, isOnlyGetCode: true, isOnlyGetToken: false, clientKeys: `${process.env.RS_TWITTER_CLIENT_KEYS as string}` });
   const { onSnapChatConnect, snapchatData, isLoading: isSnapchatLoading } = useSnapChatConnection({ clientId: process.env.RS_SNAPCHAT_CLIENT_KEY as string, clientSecret: process.env.RS_SNAPCHAT_BUSINESS_CLIENT_SECRET_KEY as string, redirect_uri: `${window.location.origin}/callback/snapchat`, isOnlyGetCode: true, isOnlyGetToken: false });
   const { onLinkedInConnect, linkedInData, isLoading: isLinkedInLoading } = useLinkedInConnection({
     clientId: process.env.RS_LINKEDIN_CLIENT_KEY as string,
@@ -16,7 +16,7 @@ function App() {
     redirectUri: `${window.location.origin}/callback/linkedin`,
     isOnlyGetToken: true
   })
-  
+
   const { onGoogleConnect, isLoading: isGoogleLoading } = useGoogleConnection({
     onSuccess: (tokenResponse: any) => {
       tokenResponse && console.log("google data", tokenResponse)
